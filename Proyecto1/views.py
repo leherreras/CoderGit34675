fa1e5030cb445388170ba18ea9734390af0e5a5f
Luis Carlos Herrera <leherreras@gmail.com>
2023-02-28 11:41:35 -0500

    first commit

---
+++ Proyecto1/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render
+
+
+def saludo(request):
+    return HttpResponse("Hola Django - Coder")
+
+
+def mostrar_html(request):
+    return HttpResponse("<button>este mi boton</button><br><h1>Hola</h1>")
+
+
+def retornar_parametro(request):
+    mensaje = f"La fecha de hoy es {datetime.now()}"
+    return HttpResponse(mensaje)
+
+
+def mi_nombre(request, nombre):
+    return HttpResponse(f"Hola, bienvenido <b>{nombre}</b>")
+
+
+def show_html(request):
+    return render(request, "template1.html")
+
+
+def show_html2(request):
+    return render(request, "template2.html")
